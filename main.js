ece24831e034a741ec4e7c966d384e6bfe7add18
Isabella Miranda <isabellamiirandam@gmail.com>
2023-08-23 09:22:11 -0300

    aplicando uma estrutura de condição

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ function handleclick(event) {
 
   const inputNumber = document.querySelector("#inputNumber")
 
+  if (Number(inputNumber) == randomNumber) {
+    document.querySelector(".screen1").classList.add('hide')
+    document.querySelector(".sreen2").classList.remove('hide')
+  }
+
   xAttemps++
 
   console.log(xAttemps)
