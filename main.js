3a4c321d072b95998cb954d3de78df0d6774c5c5
Isabella Miranda <isabellamiirandam@gmail.com>
2023-08-23 09:17:02 -0300

    criando variável de controle e incrementando

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const randomNumber = Math.round(Math.random() * 10)
 
+let xAttemps = 1
+
 function handleclick(event) {
   event.preventDefault()
 
   const inputNumber = document.querySelector("#inputNumber")
 
-  console.log(inputNumber.value)
+  xAttemps++
+
+  console.log(xAttemps)
 }
