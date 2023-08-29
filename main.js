79fae9edcbfc6e2bbbed842a8cd5e63d04f0291d
Isabella Miranda <isabellamiirandam@gmail.com>
2023-08-29 10:55:03 -0300

    refatorando o código JS

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
-/*A função Math.round() retorna o valor de um número arredondado para o inteiro mais proximo.*/
-/*A função Math.random() retorna um número pseudo-aleatório*/
+// Variáveis 
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
+/*A função Math.round() retorna o valor de um número arredondado para o inteiro mais proximo.
+A função Math.random() retorna um número pseudo-aleatório*/
 const randomNumber = Math.round(Math.random() * 10)
-
 let xAttemps = 1
 
+// Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+
+//Funções callback
 function handleTryClick(event) {
-  /* não faça o padrão do evento (não enviar o formulário)*/
-  event.preventDefault()
+
+  event.preventDefault()   //não faça o padrão do evento (não enviar o formulário)
 
   const inputNumber = document.querySelector("#inputNumber")
 
   if (Number(inputNumber.value) == randomNumber) {
-    screen1.classList.add("hide")
-    screen2.classList.remove("hide")
+    toggleScreen()
 
     document
       .querySelector(".screen2 h2")
@@ -24,20 +30,14 @@ function handleTryClick(event) {
 
   inputNumber.value = ""
   xAttemps++
-
-
 }
 
-// Eventos
-
-const btnTry = document.querySelector("#btnTry")
-const btnReset = document.querySelector("#btnReset")
-
-// Registrando os eventos 
-// addEventListener ('nome do evento', 'nome da função que será reagida')
-btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', function () {
-  screen1.classList.remove("hide")
-  screen2.classList.add("hide")
+function handleResetClick() {
+  toggleScreen()
   xAttemps = 1
-})
+}
+
+function toggleScreen() {
+  screen1.classList.toggle("hide")
+  screen2.classList.toggle("hide")
+}
