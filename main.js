bafa3f96590505a859861b7d7b39eff3884eea55
Isabella Miranda <isabellamiirandam@gmail.com>
2023-08-29 09:47:12 -0300

    criando variável e aplicando

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /*A função Math.round() retorna o valor de um número arredondado para o inteiro mais proximo.*/
 /*A função Math.random() retorna um número pseudo-aleatório*/
-
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
 const randomNumber = Math.round(Math.random() * 10)
 
 let xAttemps = 1
@@ -12,8 +13,8 @@ function handleTryClick(event) {
   const inputNumber = document.querySelector("#inputNumber")
 
   if (Number(inputNumber.value) == randomNumber) {
-    document.querySelector(".screen1").classList.add("hide")
-    document.querySelector(".screen2").classList.remove("hide")
+    screen1.classList.add("hide")
+    screen2.classList.remove("hide")
 
     document
       .querySelector(".screen2 h2")
@@ -34,3 +35,8 @@ const btnReset = document.querySelector("#btnReset")
 // Registrando os eventos 
 // addEventListener ('nome do evento', 'nome da função que será reagida')
 btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', function () {
+  screen1.classList.remove("hide")
+  screen2.classList.add("hide")
+  xAttemps = 1
+})
